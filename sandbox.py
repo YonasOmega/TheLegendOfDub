423e805f7b9579141c4d0b86457a2592f49ec576
Bill Marban <billym41620@gmail.com>
2023-11-02 10:58:00 -0700

    Made Monster classes

--- sandbox.py
+++ sandbox.py
@@ -2,6 +2,9 @@ from Model.Characters.Warrior import Warrior
 from Model.Characters.Priestess import Priestess
 from Model.Characters.Mage import Mage
 from Model.Characters.Thief import Thief
+from Model.Characters.Ogre import Ogre
+from Model.Characters.Goblin import Goblin
+from Model.Characters.Skeleton import Skeleton
 
 
 my_warrior = Warrior()
@@ -19,5 +22,13 @@ print(my_mage)
 my_thief = Thief()
 
 print(my_thief)
+
+ogre = Ogre()
+goblin = Goblin()
+skeleton = Skeleton()
+
+print(ogre)
+print(goblin)
+print(skeleton)
 
 
