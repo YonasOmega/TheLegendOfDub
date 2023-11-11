b4d119670ab6644bfa46b9a00296050bc8f29591
Bill Marban <billym41620@gmail.com>
2023-11-11 14:14:29 -0800

    Done with Making a DungeonGenerator.py

--- Model/DungeonGenerator.py
+++ Model/DungeonGenerator.py
@@ -25,6 +25,7 @@ class Dungeon:
         self.place_entrance_and_Exit()
         self.place_pillars()
         self.entrance_to_exit()
+        self.pillars_to_path()
 
     def entrance_to_exit(self):
         self.entrance_to_exit_y()
@@ -42,10 +43,16 @@ class Dungeon:
 
 
     def __str__(self):
-        # Convert each row of the maze to a string and then join all rows
-        return '\n'.join([' '.join(map(str, row)) for row in self.__Maze])
+        # First line with row and column count
+        first_line = f"{self.__Row} {self.__Col}"
 
-    ### Helper Methods
+        # Create each row of the maze
+        rows = [" ".join(str(cell) for cell in self.__Maze[i]) for i in range(self.__Row)]
+
+        # Combine first line and rows
+        return first_line + "\n" + "\n".join(rows)
+
+    # Helper Methods
     def spaced_entrance_and_exit(self):
         # Check if the exit is in any of the immediately surrounding positions of the entrance
         return (self.__EntranceX + 1, self.__EntranceY) == (self.__ExitX, self.__ExitY) \
@@ -74,6 +81,8 @@ class Dungeon:
     def place_entrance_and_Exit(self):
         self.__Maze[self.__ExitX][self.__ExitY] = 'Y'
         self.__Maze[self.__EntranceX][self.__EntranceY] = 'X'
+        self.__PathPositions.add((self.__ExitX, self.__ExitY))
+        self.__PathPositions.add((self.__EntranceX, self.__EntranceY))
 
     def entrance_to_exit_y(self):
         y_diff = self.__ExitX - self.__EntranceX
@@ -91,11 +100,49 @@ class Dungeon:
                 self.__Maze[self.__ExitX][self.__EntranceY + i] = 1
             self.__PathPositions.add((self.__ExitX, self.__EntranceY + i))
 
+    def pillars_to_path(self):
+        for pillar_pos in self.__PillarPosition:
+            nearest_path_pos = self.find_nearest_path(pillar_pos)
+            self.create_path(pillar_pos, nearest_path_pos)
+
+    def find_nearest_path(self, pillar_pos):
+        # Implement logic to find the nearest path point from the pillar
+        # This could use a simple distance calculation or a more complex pathfinding algorithm
+        nearest_path_pos = min(self.__PathPositions, key=lambda path_pos: self.distance(pillar_pos, path_pos))
+        return nearest_path_pos
+
+    def create_path(self, start_pos, end_pos):
+        # Create a path from start_pos to end_pos
+        # Mark the path in the maze and add these positions to self.__PathPositions
+
+        # Horizontal path
+        step_x = 1 if end_pos[1] > start_pos[1] else -1
+        for x in range(start_pos[1], end_pos[1] + step_x, step_x):
+            if self.__Maze[start_pos[0]][x] == 0:
+                self.__Maze[start_pos[0]][x] = 1
+                self.__PathPositions.add((start_pos[0], x))
+
+        # Vertical path
+        step_y = 1 if end_pos[0] > start_pos[0] else -1
+        for y in range(start_pos[0], end_pos[0] + step_y, step_y):
+            if self.__Maze[y][end_pos[1]] == 0:
+                self.__Maze[y][end_pos[1]] = 1
+                self.__PathPositions.add((y, end_pos[1]))
+
+    def distance(self, pos1, pos2):
+        # Calculate the Manhattan distance between two points
+        return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+
+
     def get_Pill_Pos(self):
         print(f"Pillars : {self.__PillarPosition}")
 
     def get_Path_Pos(self):
         print(f"Paths : {self.__PathPositions}")
+
+    def write_maze_to_file(self, file_name):
+        with open(file_name, 'w') as file:
+            file.write(self.__str__())
 
 # Example usage
 dungeon = Dungeon(7, 7)
@@ -103,3 +150,4 @@ dungeon.generate()
 print(dungeon)
 print(dungeon.get_Pill_Pos())
 print(dungeon.get_Path_Pos())
+dungeon.write_maze_to_file("Dungeon.txt")
