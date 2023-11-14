8c661ab936d41e791fa09a2f2b39bfb1c7936637
Bill Marban <billym41620@gmail.com>
2023-11-14 11:24:04 -0800

    Not done with Thief.py, aas truing to control the turns aloud is getting difficult.

--- Model/Characters/Thief.py
+++ Model/Characters/Thief.py
@@ -1,26 +1,26 @@
-from Model.Characters.Character import Character
+import random
+from Model.Characters.Heroes import Hero
+from Model.Characters.DungeonCharacter import DungeonCharacter
 
 
-class Thief(Character):
-    __type = "Thief"
-    __health = 4
-    __strength = 2
-    __speed = 5
-    __items = list()
+class Warrior(Hero):
+    def __init__(self, name):
+        super().__init__(name, health=75, min_damage=20, max_damage=40, attack_speed=6, chance_to_hit=0.8,
+                         chance_to_block=0.4)
 
-    def __init__(self):
-        super().__init__(self.__type, self.__health, self.__strength, self.__speed, self.__items)
+    def special_skill(self, opponent):
+        # Implement surprise attack special skill
+        surprise_attack_chance = random.randint(0, 100)
+        if surprise_attack_chance <= 20:
+            return f"{self._name} attempted a sneak attack on {opponent.get_name()}, but got caught."
+        elif surprise_attack_chance <= 60:
+            damage = self.attack(opponent)
+            return f"{self._name} used a sneak attack on {opponent.get_name()}, resulting in a normal attack of {damage}"
+        else:
+            # Increase damage for a successful surprise attack
+            self.attack(opponent)
+            return f"{self._name} successfully executed a sneak attack on {opponent.get_name()}."
 
-    def health(self):
-        print("health")
-
-    def strength(self):
-        print("attack")
-
-    def speed(self):
-        print("move")
-
-    def pull_items(self):
-        print("item")
-
-
+    def attack(self, opponent):
+        # Utilize the perform_attack method from the parent class
+        return opponent.receive_damage(self.calculate_damage())
