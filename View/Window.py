eb7aeff4fbea2e001df995a5ef9794aa4c3e546d
Bill Marban <billym41620@gmail.com>
2023-11-17 08:51:18 -0800

    Done with the bug fixes

--- View/Window.py
+++ View/Window.py
@@ -10,6 +10,7 @@ screen_height = 480
 
 # Image
 path_image = pygame.image.load("../Assets/brown_field/brown_field_detailed.png")
+path_special = pygame.image.load("../Assets/green_field/green_field_deatialed.png")
 
 # Create the window
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -24,8 +25,8 @@ pygame.display.update()
 pygame.display.set_caption("LegendOfDub")
 
 # Create a DungeonGenerator instance
-dungeon_generator = DungeonGenerator(row=10, col=10)  # Set appropriate dimensions
-dungeon_generator.generate()
+dungeon_generator = DungeonGenerator(10, 10)  # Set appropriate dimensions
+#dungeon_generator.generate()
 print(dungeon_generator)
 
 # Map the characters to their corresponding images
@@ -37,7 +38,7 @@ element_images = {
     # 'E': pillar_image,
     # 'I': pillar_image,
     # 'P': pillar_image,
-    1: path_image,
+    '1': path_image,
 }
 
 #Have a clock so we can so it to 60fps
@@ -55,6 +56,8 @@ while True:
         for col_index, element in enumerate(row):
             if element in element_images:
                 screen.blit(element_images[element], (col_index * 47, row_index * 48))
+            elif element != '0':
+                screen.blit(path_special, (col_index * 47, row_index * 48))
 
     pygame.display.update()
     clock.tick(60)  # shouldn't run more than 60 ticks
