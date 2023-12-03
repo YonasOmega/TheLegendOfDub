1777c95b09837d778c3b47d6a7b7f769e80b3de1
Bill Marban <billym41620@gmail.com>
2023-12-03 05:59:40 -0800

    Documentation for Pillars and it's subclasses

--- Model/rooms/Items/Pillar/OOP.py
+++ Model/rooms/Items/Pillar/OOP.py
@@ -1,30 +1,42 @@
 from Model.rooms.Items.Pillar.Pillar import Pillar
 
-
 class Encapsulation(Pillar):
-    # do something
+    """
+    Represents the Encapsulation pillar in the dungeon game.
+    """
+
     def __init__(self):
         super().__init__("Encapsulation")
 
 
 class Abstraction(Pillar):
-    # do something
+    """
+    Represents the Abstraction pillar in the dungeon game.
+    """
+
     def __init__(self):
         super().__init__("Abstraction")
 
 
 class Inheritance(Pillar):
-    # do something
+    """
+    Represents the Inheritance pillar in the dungeon game.
+    """
+
     def __init__(self):
         super().__init__("Inheritance")
 
 
 class Polymorphism(Pillar):
-    # Do something
+    """
+    Represents the Polymorphism pillar in the dungeon game.
+    """
+
     def __init__(self):
         super().__init__("Polymorphism")
 
 
+# Example usage
 # poly = Polymorphism()
 # print(poly.pillar)
 # poly.pillar = "value"
--- Model/rooms/Items/Pillar/Pillar.py
+++ Model/rooms/Items/Pillar/Pillar.py
@@ -1,15 +1,44 @@
 class Pillar:
+    """
+    Represents a pillar in the dungeon game.
+
+    This class encapsulates the concept of a pillar, which can be one of the foundational concepts in the game
+    (like Abstraction, Encapsulation, Inheritance, Polymorphism).
+
+    Attributes:
+        __pillar (str): A string representing the type of the pillar.
+    """
+
     def __init__(self, pillar: str):
+        """
+        Initialize a Pillar instance with a specified type.
+
+        :param pillar: A string representing the type of the pillar.
+        """
         self.__pillar = pillar
 
     @property
     def pillar(self):
+        """
+        Get the type of the pillar.
+
+        :return: The type of the pillar as a string.
+        """
         return self.__pillar
 
     @pillar.setter
     def pillar(self, value):
+        """
+        Set the type of the pillar.
+
+        :param value: A string representing the new type of the pillar.
+        """
         self.__pillar = value
 
     def __str__(self):
-        return f"{self.__pillar}"
+        """
+        Return a string representation of the Pillar object.
 
+        :return: The type of the pillar as a string.
+        """
+        return f"{self.__pillar}"
