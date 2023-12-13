d0da40530369a5940562b22392fe2a75a5fdfbc2
Bill Marban <billym41620@gmail.com>
2023-12-13 00:47:42 -0800

    Able to add, use, remove, and peek bag

--- Model/Characters/hero/Heroes.py
+++ Model/Characters/hero/Heroes.py
@@ -7,6 +7,7 @@ class Hero(DungeonCharacter):
     def __init__(self, name, health, min_damage, max_damage, attack_speed, chance_to_hit, chance_to_block):
         super().__init__(name, health, min_damage, max_damage, attack_speed, chance_to_hit)
         self.chance_to_block = chance_to_block
+        self.__bag = []
 
     @abstractmethod
     def special_skill(self, opponent):
@@ -56,3 +57,47 @@ class Hero(DungeonCharacter):
             print(f"{self._name} missed the attack on {opponent.get_name()}.")
         self.__str__()
         opponent.__str__()
+
+    def put_in_bag(self, item):
+        self.__bag.append(item)
+
+    @property
+    def bag(self):
+        if len(self.__bag) == 0:
+            return "Empty bag"
+        output = "Bag: [ "
+        for i in self.__bag:
+            output += str(i) + ", "  # Ensure each item is converted to string
+        output += "]"
+        return output
+
+    def use_item(self, item_index):
+        item_index = item_index - 1
+        """ Use an item from the bag """
+        if 0 <= item_index < len(self.__bag):
+            item = self.__bag[item_index]
+            # Apply the item's effect here. This depends on how items are structured.
+            # For example: item.apply_effect(self)
+            item.effect(self)
+            self.remove_item(item_index)
+            print(f"Used {item.__str__()}.")
+
+    def remove_item(self, item_index):
+        """ Remove an item from the bag """
+        if 0 <= item_index < len(self.__bag):
+            removed_item = self.__bag.pop(item_index)
+            print(f"Removed {removed_item} from bag.")
+
+    def player_use_item(self):
+        print(self.bag)  # Display the contents of the bag
+
+        try:
+            choice = int(input("Select an item to use (1-based index) or 0 to cancel: "))
+            if choice == 0:
+                print("No item selected.")
+            else:
+                self.use_item(choice)
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+
--- sandbox.py
+++ sandbox.py
@@ -2,6 +2,25 @@ from Model.Characters.Monsters.Monster import Gremlin
 #from Model.Characters.Monsters.Gremlin import Gremlin
 from Model.Characters.Monsters.Monster import Skeleton
 from Model.Characters.Monsters.Monster import Ogre
+from Model.Characters.hero.Warrior import Warrior
+from Model.rooms.Items.potion.Health_Potion import Health_Potion
+from Model.rooms.Items.Pillar.OOP import Abstraction
+from Model.rooms.Items.Pillar.OOP import Polymorphism
+from Model.rooms.Items.Pillar.OOP import Encapsulation
+from Model.rooms.Items.Pillar.OOP import Inheritance
 
-grem = Ogre()
-print(grem)
+hero = Warrior("Warrior")
+item = Health_Potion()
+poly = Polymorphism()
+Abs = Abstraction()
+Enc = Encapsulation()
+Inh = Inheritance()
+
+print(hero)
+print(hero.bag)
+
+hero.put_in_bag(poly)
+hero.put_in_bag(Abs)
+hero.put_in_bag(Enc)
+hero.put_in_bag(Inh)
+print(hero.bag)
