5ebdefa4518ed7ea1a610ed15bdc71a1128f218a
Bill Marban <billym41620@gmail.com>
2023-12-15 20:28:04 -0800

    Controller,py documentation

--- Controller/Controller.py
+++ Controller/Controller.py
@@ -3,18 +3,33 @@ import pygame
 
 class PlayerController:
     """
-    Initializes the PlayerController.
+    Manages the player's movement, animation, and interactions within the game.
 
-    Parameters:
-    - start_pos: Starting position of the player (x, y).
-    - size: Size of the player.
-    - speed: Speed of the player's movement.
-    - heroes_model: Reference to the heroes model.
-    - dungeon_generator: Reference to the dungeon generator.
-    - assets: Dictionary containing player animation assets for different directions.
+    Attributes:
+        position (list): The current position of the player in (x, y) coordinates.
+        size (tuple): The size of the player.
+        speed (int): The movement speed of the player.
+        heroes_model (Hero): Reference to the heroes model.
+        dungeon_generator (DungeonGenerator): Reference to the dungeon generator.
+        god_mode (bool): Indicates whether the god mode is activated.
+        assets (dict): Assets for player's animation in different directions.
+        current_direction (str): The current direction of the player.
+        current_frame (int): The current animation frame.
+        frame_counter (int): Counter for controlling animation speed.
+        __player (Player): Reference to the player object.
     """
-
     def __init__(self, start_pos, size, speed, heroes_model, dungeon_generator, assets, player):
+        """
+        Initializes the PlayerController.
+
+        Parameters:
+        - start_pos: Starting position of the player (x, y).
+        - size: Size of the player.
+        - speed: Speed of the player's movement.
+        - heroes_model: Reference to the heroes model.
+        - dungeon_generator: Reference to the dungeon generator.
+        - assets: Dictionary containing player animation assets for different directions.
+        """
         self.position = start_pos
         self.size = size
         self.speed = speed
@@ -27,14 +42,13 @@ class PlayerController:
         self.frame_counter = 0
         self.__player = player
 
-    """
-    Moves the player in the specified direction.
-
-    Parameters:
-    - direction: The direction in which the player should move ('UP', 'DOWN', 'LEFT', 'RIGHT').
-    """
-
     def move(self, direction):
+        """
+        Moves the player in the specified direction.
+
+        Parameters:
+        - direction: The direction in which the player should move ('UP', 'DOWN', 'LEFT', 'RIGHT').
+        """
         new_position = self.position.copy()
         move_x = 47  # Horizontal movement increment (width of one cell)
         move_y = 47  # Vertical movement increment (height of one cell)
@@ -58,14 +72,28 @@ class PlayerController:
 
     # Gets the current position of the player
     def get_position(self):
+        """
+        Returns the current position of where the controller is
+        :return:
+        """
         return self.position
 
     def get_grid_position(self):
+        """
+        Returns the current grid position of where the controller is
+        :return:
+        """
         grid_position = (self.position[1] // 47, self.position[0] // 47)
         return grid_position
 
     # Updates the player position based of the key state
     def update(self, key_state):
+        """
+        Updates the player's position based on the current key state.
+
+        Parameters:
+            key_state (dict): Dictionary representing the state of keys.
+        """
         # First, handle the God Mode toggle
         if key_state[pygame.K_g]:
             if not self.key_held:  # Check if key is not already held down
@@ -103,45 +131,52 @@ class PlayerController:
                 self.move("RIGHT")
                 self.key_held = True
 
-    """
-           Updates the player based on the given key state.
-
-           Parameters:
-           - key_state: Dictionary representing the state of keys (True if pressed, False otherwise).
-           """
-
     def update_animation(self):
+        """
+        Updates the player's animation frame for smooth movement.
+        """
         self.frame_counter += 1
         if self.frame_counter >= 60:  # FRAME_DELAY controls the speed of the animation
             self.frame_counter = 0
             self.current_frame = (self.current_frame + 1) % len(self.assets[self.current_direction])
 
-    """
-    Sets the new speed for the player. (optional)
-
-    Parameters:
-    - new_speed: The new speed value.
-    """        """
-        Sets the new speed for the player.
+    def set_speed(self, new_speed):  # We can use to update speed for boost
+        """
+        Sets the new speed for the player. (optional)
 
         Parameters:
         - new_speed: The new speed value.
-        """
+        """        """
+            Sets the new speed for the player.
 
-    def set_speed(self, new_speed):  # We can use to update speed for boost
+            Parameters:
+            - new_speed: The new speed value.
+            """
         self.speed = new_speed
 
     @property
     def god_mode(self):
+        """
+        Getter for god mode attribute.
+        :return: God mode
+        """
         return self.god_mode
 
     @god_mode.setter
     def god_mode(self, value):
+        """
+        Sets the value for the god_mode attribute
+        :param value: the value god is going to be
+        """
         self._god_mode = value
 
-    #TODO
-    # FINilizre
     def update_visibility(self, visibility):
+        """
+        Updates the visibility grid based on the player's current position.
+
+        Parameters:
+            visibility (list of lists): A 2D list representing the visibility grid.
+        """
         x, y = self.__player.position
         visibility[x][y] = True
         # Optionally, reveal adjacent tiles as well
