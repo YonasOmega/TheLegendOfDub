68b6438e43750603379c9d1439a3bd4be321aa01
Bill Marban <billym41620@gmail.com>
2023-12-15 20:21:00 -0800

    Pillars

--- Model/rooms/Items/Pillar/OOP.py
+++ Model/rooms/Items/Pillar/OOP.py
@@ -2,24 +2,49 @@ from Model.rooms.Items.Pillar.Pillar import Pillar
 
 
 class Encapsulation(Pillar):
-    # do something
+    """
+    Represents the Encapsulation pillar in the game.
+    Inherits from the Pillar class.
+    """
     def __init__(self):
+        """
+        Initializes an Encapsulation pillar instance.
+        """
         super().__init__("Encapsulation")
 
 
 class Abstraction(Pillar):
-    # do something
+    """
+    Represents the Abstraction pillar in the game.
+    Inherits from the Pillar class.
+    """
     def __init__(self):
+        """
+        Initializes an Abstraction pillar instance.
+        """
         super().__init__("Abstraction")
 
 
 class Inheritance(Pillar):
-    # do something
+    """
+    Represents the Inheritance pillar in the game.
+    Inherits from the Pillar class.
+    """
     def __init__(self):
+        """
+        Initializes an Inheritance pillar instance.
+        """
         super().__init__("Inheritance")
 
 
 class Polymorphism(Pillar):
+    """
+    Represents the Polymorphism pillar in the game.
+    Inherits from the Pillar class.
+    """
     # Do something
     def __init__(self):
+        """
+        Initializes a Polymorphism pillar instance.
+        """
         super().__init__("Polymorphism")
--- Model/rooms/Items/Pillar/Pillar.py
+++ Model/rooms/Items/Pillar/Pillar.py
@@ -1,15 +1,46 @@
 class Pillar:
+    """
+    Represents a pillar in the game. Each pillar symbolizes a key concept of object-oriented programming.
+
+    Attributes:
+        __pillar (str): The name of the pillar.
+    """
+
     def __init__(self, pillar: str):
+        """
+        Initializes a Pillar instance.
+
+        Parameters:
+            pillar (str): The name of the pillar.
+        """
         self.__pillar = pillar
 
     @property
     def pillar(self):
+        """
+        Property to get the name of the pillar.
+
+        Returns:
+            str: The name of the pillar.
+        """
         return self.__pillar
 
     @pillar.setter
     def pillar(self, value):
+        """
+        Property setter to set the name of the pillar.
+
+        Parameters:
+            value (str): The new name of the pillar.
+        """
         self.__pillar = value
 
     def __str__(self):
+        """
+        String representation of the Pillar.
+
+        Returns:
+            str: The name of the pillar.
+        """
         return f"{self.__pillar}"
 
